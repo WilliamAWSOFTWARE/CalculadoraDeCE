c50f3e7a2a6b9149a16c8f7af2a57731976d06df
WilliamAWSOFTWARE <45134675+WilliamAWSOFTWARE@users.noreply.github.com>
2019-07-29 19:32:15 -0400

    Se agregaron el guardar datos

--- functions.c
+++ functions.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-int ndn=0,i=0,coordenadas[0],fil,col;
+#include <malloc.h>
+#include "functions.h"
+#include <windows.h>
+
+int ndn=0,i=0,coordenadas[0],fil,col,totno;
+char character[21];
 void asignar_potenciales();
 void asignar_nodos();
 //LEY DE SUMATORIA DE VOLTAJE = 0 DE KIRCHHOFF
@@ -35,52 +42,115 @@ float calcular_porcentaje_de_regulacion(float vt, float vr){
 float divisores_de_voltaje(float r[], float i);
 float divisores_de_corriente(float r[], float v);
 float resistencia_equivalente(float r[]);
-int detectar_nodos(char grafica[3][3], int tc, int tf){
+int detectar_nodos(char grafica[][20], int tm){
     //Aqui se revisa la matriz completa con sus valores
-     for(fil=0;fil<=2;fil++){
-        for(col=0;col<=2;col++){
+    totno=0;
+    for(fil=0;fil<tm;fil++){
+        for(col=0;col<tm;col++){
             if(grafica[fil][col]=='O'){
-                //printf("HAY UN MALDITO NODO\n");
-                if (grafica[fil][col+1]=='R' || grafica[fil][col+1]=='F'){
-                    //printf("A LA DERECHA DEL NODO\n");
+                printf("HAY UN NODO\n");
+                if (grafica[fil][col-1]=='R' || grafica[fil][col-1]=='F'){
+                    printf("\nA LA IZQUIERDA DEL NODO\n");
                     ndn++;
                 }
                 //Se Compara hacia abajo a 2 espacios, del potencial
                 if (grafica[fil+1][col]=='R' || grafica[fil+1][col]=='F'){
-                    //printf("ARRIBA DEL NODO\n");
+                    printf("ABAJO DEL NODO\n");
                     ndn++;
                 }
                 //Se Compara por la izquierda a 2 espacios, del potencial
-                if (grafica[fil][col-1]=='R' || grafica[fil][col-1]=='F'){
-                    //printf("A LA IZQUIERDA DEL NODO\n");
+                if (grafica[fil][col+1]=='R' || grafica[fil][col+1]=='F'){
+                    printf("A LA DERECHA DEL NODO\n");
                     ndn++;
                 }
                 //Se Compara por arriba a 2 espacios, del potencial
                 if (grafica[fil-1][col]=='R' || grafica[fil-1][col]=='F'){
-                    //printf("ABAJO DEL NODO\n");
+                    printf("ARRIBA DEL NODO\n");
                     ndn++;
                 }
+                printf("\nVALOR DE NDN %d\n",ndn);
+                if(ndn>2){
+                    totno++;
+                    ndn=0;
+                }
+                ndn=0;
             }
+
         }
     }
-    return ndn;
+    return totno;
 }
-char dibujar_nodo(char g[0][0],int cx, int cy){
-
+char dibujar_nodo(char g[][20],int cx, int cy){
+    if(g[cx][cy]!='0'){
+        return 'O';
+    }else{
+        return '0';
+    }
 }
-
-char dibujar_fuente_voltaje(char g[][0],int cx, int cy){
-    if(g[cx][cy]=='X'){
+char dibujar_fuente_voltaje(char g[][20],int cx, int cy){
+    if(g[cx][cy]!='0'){
         return 'F';
     }else{
         return '0';
     }
 }
-
-char dibujar_resistencia(char g[0][0],int cx, int cy){
-    if(g[cx][cy]=='X'){
+char dibujar_resistencia(char g[][20],int cx, int cy){
+    if(g[cx][cy]!='0'){
         return 'R';
     }else{
         return '0';
     }
 }
+void dibujar_grafica(char grafica[][20],int tm){
+    system("CLS");
+    printf("CIRCUITO DEFAULT\n");
+    for(fil=0;fil<tm;fil++){
+    for(col=0;col<tm;col++){
+        if(grafica[fil][col]=='0'){
+            SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),15);
+        }else if(grafica[fil][col]=='X'){
+            SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),8);
+        }else if(grafica[fil][col]=='R' || grafica[fil][col]=='r'){
+            SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),3);
+        }else if(grafica[fil][col]=='F' || grafica[fil][col]=='f'){
+            SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),2);
+        }else if(grafica[fil][col]=='O' || grafica[fil][col]=='o'){
+            SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),1);
+        }else if(grafica[fil][col]=='E'){
+            SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),105);
+        }else if(grafica[fil][col]==124|| grafica[fil][col]==196){
+            SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),9);
+        }
+
+        if(col==0){
+            if(fil<=9){
+                printf("0%d %c ",fil,grafica[fil][col]);
+            }else{
+                printf("%d %c ",fil,grafica[fil][col]);
+            }
+        }else{
+            printf("%c ",grafica[fil][col]);
+        }
+    }
+    printf("\n");
+}
+}
+char dibujar_cable(char g[][20],int cx, int cy,int type){
+    if(g[cx][cy]!='0'){
+        if(type==1){
+            return 124;
+        }else if(type==2){
+            return '-';
+        }
+    }else{
+        return '0';
+    }
+}
+void guarda_circuito(char grafica[][20],FILE * circuito){
+        circuito=fopen("circuito.txt","w");
+        circuito=fopen("circuito.txt","a+t");
+        fputs(grafica,circuito);
+        fclose(circuito);
+}
+
+
--- main.c
+++ main.c
@@ -3,97 +3,114 @@
 #include <math.h>
 #include <malloc.h>
 #include "functions.h"
+#include <windows.h>
 
 
-int fil,col,i,tm, cx,cy;
-char grafica[0][0];
+
+int fil,col,i,tm=20, cx,cy,pos,resp;
+char grafica[0][0],temp, * nombre, character[21];
 int main()
+
 {
-
+FILE * circuito;
 //EL USUARIO DEFINE EL TAMAÑO DE LA MATRIZ
-do{
-
-    printf("Actualmente no se soporta un tama%co mayor a 50!\n",164);
-    printf("Actualmente no se soporta un tama%co menor a 10!",164);
-    printf("\nDefina el tama%co de la matriz(No mayor a 50):\n",164);
+/*do{
+    printf("Actualmente no se soporta un tama%co mayor a 20! ni menor a 10!",164);
+    printf("\nDefina el tama%co de la matriz(No mayor a 20, no menor a 10):\n",164);
     scanf("%d",&tm);
-}while(tm>50 && tm<10);
+}while(tm<10 || tm>20);*/
 //Si EL RESULTADO QUE SALE NO ES CORRECTO, SE VALIDA AHI ARRIBA ^^^^^^^
-
 char grafica[tm][tm];
-
 //AQUI SE LE ASIGNAN VALORES POR DEFAULT LA MATRIZ
-for(fil=0;fil<tm;fil++){
-    for(col=0;col<tm;col++){
-        if(fil==0 && col>=0 && col<=tm-1){
-            grafica[fil][col]='0';
-        }else if(fil!=0 && col==0){
-            grafica[fil][col]='0';
-        }else if(fil==tm-1 && col==tm-1){
-            grafica[fil][col]='0';
-        }else if(fil==tm-1 && col!=tm-1){
-            grafica[fil][col]='0';
-        }else if(col==tm-1 && fil>=0 && fil<=tm-1){
-            grafica[fil][col]='0';
-        }else{
-            grafica[fil][col]='X';
-        }
+circuito=fopen("circuito.txt","r");
+if(circuito==NULL){
+    MessageBox(NULL,"Aviso","No existe un archivo de circuito!!!\nSe va a proceder a crearlo!",MB_OK | MB_ICONSTOP);
+    //printf("No existe un archivo de circuito!!!\nSe va a proceder a crearlo\n");
+    system("PAUSE");
+    //SE CREARA EL ARCHIVO DONDE SE VA A INGRESAR EL CIRCUITO
+    circuito=fopen("circuito.txt","w");
+    fclose(circuito);
 
+}else{
+    printf("El archivo existe!!\nDesea modificarlo o sobreescribirlo?\n\n1.Modificar\n2.Eliminar y abrir nuevo\n\nRespuesta:");
+    scanf("%d",&resp);
+    if(resp==1){
+        circuito=fopen("circuito.txt","r");
+        fgets(grafica,(tm*tm)+1,circuito);
+
+    }else if(resp==2){
+        circuito=fopen("circuito.txt","w+t");
+
+        for(fil=0;fil<tm;fil++){
+            for(col=0;col<tm;col++){
+                if(fil==0 && col>=0 && col<=tm-1){
+                    grafica[fil][col]='0';
+                }else if(fil!=0 && col==0){
+                    grafica[fil][col]='0';
+                }else if(fil==tm-1 && col==tm-1){
+                    grafica[fil][col]='0';
+                }else if(fil==tm-1 && col!=tm-1){
+                    grafica[fil][col]='0';
+                }else if(col==tm-1 && fil>=0 && fil<=tm-1){
+                    grafica[fil][col]='0';
+                }else{
+                    grafica[fil][col]='X';
+                }
+            }
+        }
+        circuito=fopen("circuito.txt","a+t");
+        fputs(grafica,circuito);
+        fclose(circuito);
     }
 }
-//LIMPIEZA PARA QUE SOLO SE VEA El CIRCUITO DIBUJADO
-system("CLS");
-printf("\nDonde empezara a dibujar?(Tome en cuenta que no podra cambiar la posicion en donde se encuentra el elemento de circuito)\n");
-printf("Es obligatorio dibujar primero la/s fuente!!!");
-printf("Coordenadas en X:");
-scanf("%d",&cx);
-printf("Coordenadas en Y:");
-scanf("%d",&cy);
+fclose(circuito);
 
 
-//SE VALIDA QUE EL RESULTADO DE DIBUJAR NO SEA INCORRECTO
-/*do{
-    printf("Coordenadas en X:");
-    scanf("%d",&cx);
-    printf("Coordenadas en Y:");
-    scanf("%d",&cy);
-    if(dibujar_fuente_voltaje(grafica,cx,cy)!='0'){
-        grafica[cx][cy]=dibujar_fuente_voltaje(grafica,cx,cy);
-    }else{
-        printf("\nLas coordenadas ingresadas no son validas!!!\n");
-    }
-}while(dibujar_fuente_voltaje(grafica,cx,cy)=='0');*/
+
 
 
 
 //SE IMPRIME EL CIRCUITO
+dibujar_grafica(grafica, tm);
+//INICIO DEL PROGRAMA
 do{
-    printf("CIRCUITO DEFAULT\n");
-    for(fil=0;fil<tm;fil++){
-    for(col=0;col<tm;col++){
-        printf("%c ",grafica[fil][col]);
+//SE LE ASIGNA COLORES
+    SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),7);
+    printf("\nDonde desea dibujar?");
+    printf("\nCoordenadas en X:");
+    scanf("%d",&cx);
+    printf("Coordenadas en Y:");
+    scanf("%d",&cy);
+    //ESTA VARIABLE TEMPORAL GUARDA EL VALOR POR SI LUEGO SE CANCELA LA ACCION
+    temp=grafica[cx][cy];
+    grafica[cx][cy]='E';
+    system("CLS");
+    dibujar_grafica(grafica, tm);
+    printf("En la posicion escojida, elija que poner:\n");
+    printf("\n1.Resistencia (/%c/%c/) \n2.Fuente de voltaje ([+-])\n3.Potencial (o) o Nodo (o)\n4.Cable\n5.Cancelar",92,92,92);
+    printf("\n\nRespuesta:");
+    scanf("%d",&resp);
+    if (resp==1){
+        grafica[cx][cy]=dibujar_resistencia(grafica,cx,cy);
+    }else if(resp==2){
+        grafica[cx][cy]=dibujar_fuente_voltaje(grafica,cx,cy);
+    }else if(resp==3){
+        grafica[cx][cy]=dibujar_nodo(grafica,cx,cy);
+    }else if(resp==4){
+        printf("\n1.Cable (%c)\n2.Cable(-)",124);
+        printf("\n\nRespuesta:");
+        scanf("%d",&resp);
+        grafica[cx][cy]=dibujar_cable(grafica,cx,cy,resp);
+    }else if(resp==5){
+        grafica[cx][cy]=temp;
+
     }
-    printf("\n");
-}
-printf("\nDonde empezara a dibujar?(Tome en cuenta que no podra cambiar la posicion en donde se encuentra el elemento de circuito)\n");
+    system("CLS");
+    //SE VUELVE A IMPRIMIR EL CIRCUITO
+    dibujar_grafica(grafica, tm);
+    guarda_circuito(grafica,circuito);
+    printf("Precione CTRL+C para terminar la ejecucion del programa y guardar los archivos");
 
-printf("Coordenadas en X:");
-scanf("%d",&cx);
-printf("Coordenadas en Y:");
-scanf("%d",&cy);
-
-//Metodo temporal
-printf("\n1.Resistencia n2.Fuente de voltaje [+-]");
-int resp;
-printf("\n\nRespuesta:");
-scanf("%d",&resp);
-if (resp==1){
-    grafica[cx][cy]='R';
-}else{
-    grafica[cx][cy]='F';
-}
-system("CLS");
-
-}while(1);
+}while(resp!=0);
 
 }
